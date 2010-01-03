5f0f7827a7fd08e4e074633757d90bb5bca41422
t0rx <t0rxon@googlemail.com>
2010-01-03 14:25:31 +0000

    [FRN-28] First cut of a sample client-server app showing using messages across a socket.  Currently works with XML, but need to switch to proper Fudge encoding.

---
+++ Samples/SocketCommandSample/Client/Program.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * <!--
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc. and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -->
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using Fudge.Encodings;
+using System.Threading;
+using Fudge;
+using System.IO;
+
+namespace Client
+{
+    /// <summary>
+    /// Example program that connects to a server and sends it commands using FudgeMsgs.
+    /// </summary>
+    class Program
+    {
+        private FudgeContext context = new FudgeContext();
+        private Socket socket;
+        private bool closed;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Syntax: client.exe host port");
+                return;
+            }
+            new Program().Run(args[0], int.Parse(args[1]));
+        }
+
+        private void Run(string host, int port)
+        {
+            // Connect to the server
+            var stream = Connect(host, port);
+
+            // Start the thread to receive messages from the server
+            new Thread(() => { RunReceiver(stream); }).Start();
+
+            // Create our output writer onto the stream to send messages to the server
+            var output = new FudgeXmlStreamWriter(stream, "msg") { AutoFlushOnMessageEnd = true };
+
+            // Now just loop getting input from the console until we're done
+            while (!closed)
+            {
+                string line = Console.ReadLine();
+                if (!closed)
+                    SendCommand(output, line);
+            }
+        }
+
+        private void SendCommand(IFudgeStreamWriter output, string commandLine)
+        {
+            // Package the input into a message and send it
+            var msg = new FudgeMsg(context);
+
+            var bits = commandLine.Split(' ');
+            msg.Add("command", bits[0]);
+
+            for (int i = 1; i < bits.Length; i++)
+            {
+                msg.Add("args", bits[i]);
+            }
+
+            output.WriteMsg(msg);
+        }
+
+        /// <summary>
+        /// Receives messages from the server and handles them.
+        /// </summary>
+        /// <param name="stream"></param>
+        private void RunReceiver(Stream stream)
+        {
+            var inputReader = new FudgeXmlStreamReader(stream);                 // We're reading XML from the stream...
+            var messages = new FudgeMsgStreamWriter(context);                   // ...and turning it into messages...
+            var pipe = new FudgeStreamPipe(inputReader, messages);              // ...as they arrive using a pipe...
+            pipe.MessageProcessed += () => { HandleMessage(pipe, messages); };    // ...then processing them.
+
+            // Run until there's no more data
+            pipe.Process();
+        }
+
+        private void HandleMessage(FudgeStreamPipe pipe, FudgeMsgStreamWriter messageSource)
+        {
+            // Get the message we've just received and output it
+            var msg = messageSource.DequeueMessage();
+
+            switch (msg.GetString("status"))
+            {
+                case "success":
+                    Console.WriteLine("[" + msg.GetString("output") + "]");
+                    break;
+                case "fail":
+                    Console.WriteLine("[FAIL: " + msg.GetMessage("error") + "]");
+                    break;
+                case "close":
+                    // Server closing the connection
+                    Console.WriteLine("[Server sent close]");
+                    pipe.Abort();           // So it won't try to read any more data
+                    closed = true;
+                    socket.Close();
+                    break;
+                default:
+                    Console.WriteLine("[Unknown message from server:\n" + msg.ToString() + "\n]");
+                    break;
+            }
+        }
+
+        private NetworkStream Connect(string host, int port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(host, port);
+            Console.WriteLine("Connected to " + socket.RemoteEndPoint + " local endpoint is " + socket.LocalEndPoint);
+
+            var stream = new NetworkStream(socket);
+            return stream;
+        }
+    }
+}
---
+++ Samples/SocketCommandSample/Server/Program.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * <!--
+ * Copyright (C) 2009 - 2010 by OpenGamma Inc. and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -->
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using Fudge.Encodings;
+using Fudge;
+
+namespace Server
+{
+    /// <summary>
+    /// Example program that runs commands sent to it as FudgeMsgs from a client.
+    /// </summary>
+    class Program
+    {
+        private FudgeContext context;
+        private bool shutDown;
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        void Run()
+        {
+            context = new FudgeContext();
+
+            using (Socket socket = StartListening())
+            {
+                while (!shutDown)
+                {
+                    // Wait for a new connection to process
+                    using (var connectionSocket = socket.Accept())
+                    {
+                        // Now handle it (note we won't accept any others until we're done with this one
+                        ProcessConnectionWithLoop(connectionSocket);
+                    }
+                }
+            }
+        }
+
+        private void ProcessConnectionWithLoop(Socket socket)
+        {
+            Console.WriteLine("Got connection from " + socket.RemoteEndPoint);
+            try
+            {
+                // Create our output channel and send a welcome message
+                var stream = new NetworkStream(socket);
+                var output = new FudgeXmlStreamWriter(stream, "msg") { AutoFlushOnMessageEnd = true };
+                SendSuccess(output, "Welcome to " + socket.LocalEndPoint + ", type ? for help");
+
+                // Create an input channel
+                var input = new FudgeXmlStreamReader(stream);
+
+                while (true)
+                {
+                    var msg = input.ReadMsg();            // This will block until a whole message is read
+                    if (msg == null)
+                    {
+                        // Done
+                        Console.WriteLine("Data from " + socket.RemoteEndPoint.ToString() + " finished.");
+                        break;
+                    }
+                    if (!ProcessMessage(msg, output, socket))
+                    {
+                        // Done
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private bool ProcessMessage(FudgeMsg msg, IFudgeStreamWriter writer, Socket socket)
+        {
+            // We've received a message, so deal with it
+
+            bool keepGoing = true;
+            string command = msg.GetString("command");
+            Console.WriteLine("Received command " + command);
+            if (command == null)
+            {
+                SendError(writer, "No command found");
+            }
+            else
+            {
+                switch (command.ToLower())
+                {
+                    case "?":
+                    case "help":
+                        DoHelp(writer);
+                        break;
+                    case "echo":
+                        DoEcho(writer, msg);
+                        break;
+                    case "exit":
+                        SendSuccess(writer, "Closing connection.");
+                        SendClose(writer);
+                        Console.WriteLine("Closing connection from " + socket.RemoteEndPoint.ToString());
+                        socket.Close();
+                        keepGoing = false;
+                        break;
+                    case "shutdown":
+                        SendSuccess(writer, "Shutting down.");
+                        SendClose(writer);
+                        socket.Close();
+                        Console.WriteLine("Shutting down.");
+                        shutDown = true;
+                        keepGoing = false;
+                        break;
+                    case "time":
+                        DoTime(writer);
+                        break;
+                    default:
+                        SendError(writer, "Unknown command: " + command);
+                        break;
+                }
+            }
+            return keepGoing;
+        }
+
+        private void DoTime(IFudgeStreamWriter writer)
+        {
+            SendSuccess(writer, DateTime.Now.ToString("u"));
+        }
+
+        private void DoEcho(IFudgeStreamWriter writer, FudgeMsg msg)
+        {
+            var argFields = msg.GetAllByName("args");
+            string returnMsg;
+            if (argFields.Count == 0)
+                returnMsg = "echo";
+            else
+            {
+                string[] args = argFields.Select(field => field.Value.ToString()).ToArray();
+                returnMsg = string.Join(" ", args);
+            }
+            SendSuccess(writer, returnMsg);
+        }
+
+        private void DoHelp(IFudgeStreamWriter writer)
+        {
+            string message = "Available commands:\n";
+            message += "  ?         - Show help\n";
+            message += "  echo text - Send text back to client\n";
+            message += "  exit      - Close the client\n";
+            message += "  shutdown  - Shut the server down\n";
+            message += "  time      - Show the current time\n";
+            SendSuccess(writer, message);
+        }
+
+        private void SendSuccess(IFudgeStreamWriter writer, string message)
+        {
+            var msg = new FudgeMsg(context, new Field("status", "success"),
+                                            new Field("output", message));
+            writer.WriteMsg(msg);
+        }
+
+        private void SendClose(IFudgeStreamWriter writer)
+        {
+            var msg = new FudgeMsg(context, new Field("status", "close"));
+            writer.WriteMsg(msg);
+        }
+
+        private void SendError(IFudgeStreamWriter writer, string message)
+        {
+            var msg = new FudgeMsg(context, new Field("status", "fail"),
+                                            new Field("error",
+                                                new Field("message", message)));
+            writer.WriteMsg(msg);
+        }
+
+        static Socket StartListening()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Listen on all network interfaces, using any free port
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+            socket.Bind(ep);
+            socket.Listen(0);       // Don't allow any pending connections if we've got one
+
+            Console.WriteLine("Listening on port {0}...", ((IPEndPoint)socket.LocalEndPoint).Port);
+            return socket;
+        }
+    }
+}
